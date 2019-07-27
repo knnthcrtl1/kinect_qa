578951754a43311bd9cec6d775f4510a37e720b6
kenneth <knnthcrtl.kc@gmail.com>
2019-07-27 20:35:25 +0800

    Updates

---
+++ precache-manifest.31aef7bf2fc4261a700653dac6f0a094.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e63d454001b4eecbfdda74c4add6e47",
+    "url": "/knnthcrtl1/kinect_qa/index.html"
+  },
+  {
+    "revision": "ea34eb3899a25111d265",
+    "url": "/knnthcrtl1/kinect_qa/static/css/3.d432a4d4.chunk.css"
+  },
+  {
+    "revision": "8629e8661e00179954fe",
+    "url": "/knnthcrtl1/kinect_qa/static/css/4.0156f633.chunk.css"
+  },
+  {
+    "revision": "e8c967114fc2c0a48942",
+    "url": "/knnthcrtl1/kinect_qa/static/css/5.df62bbc8.chunk.css"
+  },
+  {
+    "revision": "d28e6d8c6b21eda89e31",
+    "url": "/knnthcrtl1/kinect_qa/static/css/6.fc028217.chunk.css"
+  },
+  {
+    "revision": "f29346b3c0b207704e9b",
+    "url": "/knnthcrtl1/kinect_qa/static/css/7.631fda43.chunk.css"
+  },
+  {
+    "revision": "fb06d45cca4c5d2bb4a5",
+    "url": "/knnthcrtl1/kinect_qa/static/css/8.cbd4e793.chunk.css"
+  },
+  {
+    "revision": "81962dfc2becef1aac90",
+    "url": "/knnthcrtl1/kinect_qa/static/css/9.e95a3574.chunk.css"
+  },
+  {
+    "revision": "70a2b579dcf01ac35ef2",
+    "url": "/knnthcrtl1/kinect_qa/static/css/main.e5f61648.chunk.css"
+  },
+  {
+    "revision": "4c2cbac85dfb7344c4ae",
+    "url": "/knnthcrtl1/kinect_qa/static/js/2.78f5810d.chunk.js"
+  },
+  {
+    "revision": "ea34eb3899a25111d265",
+    "url": "/knnthcrtl1/kinect_qa/static/js/3.a4d6497d.chunk.js"
+  },
+  {
+    "revision": "8629e8661e00179954fe",
+    "url": "/knnthcrtl1/kinect_qa/static/js/4.98b5ab60.chunk.js"
+  },
+  {
+    "revision": "e8c967114fc2c0a48942",
+    "url": "/knnthcrtl1/kinect_qa/static/js/5.58bd472d.chunk.js"
+  },
+  {
+    "revision": "d28e6d8c6b21eda89e31",
+    "url": "/knnthcrtl1/kinect_qa/static/js/6.d0e1b82f.chunk.js"
+  },
+  {
+    "revision": "f29346b3c0b207704e9b",
+    "url": "/knnthcrtl1/kinect_qa/static/js/7.a8e5f4ea.chunk.js"
+  },
+  {
+    "revision": "fb06d45cca4c5d2bb4a5",
+    "url": "/knnthcrtl1/kinect_qa/static/js/8.7024cb24.chunk.js"
+  },
+  {
+    "revision": "81962dfc2becef1aac90",
+    "url": "/knnthcrtl1/kinect_qa/static/js/9.248d3766.chunk.js"
+  },
+  {
+    "revision": "70a2b579dcf01ac35ef2",
+    "url": "/knnthcrtl1/kinect_qa/static/js/main.7fb61a44.chunk.js"
+  },
+  {
+    "revision": "80e6b5cdfda3f937cc64",
+    "url": "/knnthcrtl1/kinect_qa/static/js/runtime~main.58f15cbc.js"
+  },
+  {
+    "revision": "e570262b45a44a4fcabf2831a63b2baf",
+    "url": "/knnthcrtl1/kinect_qa/static/media/agent_skill.e570262b.jpg"
+  },
+  {
+    "revision": "78059ffbd10f9b3bd35b03a0127c68c4",
+    "url": "/knnthcrtl1/kinect_qa/static/media/agent_skill_assignment.78059ffb.jpg"
+  },
+  {
+    "revision": "46a7d48240d428c9dc3d4ff579199312",
+    "url": "/knnthcrtl1/kinect_qa/static/media/poppins.46a7d482.woff"
+  }
+]);
